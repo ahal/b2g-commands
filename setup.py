b0c3a1de8f0a497ce6221527990df3ae978daed5
Andrew Halberstadt <halbersa@gmail.com>
2013-11-22 11:23:20 -0500

    Add setup.py and clobber command

---
+++ setup.py
@@ -0,0 +1,31 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup
+
+PACKAGE_VERSION = '0.1'
+
+deps = ['mach']
+
+setup(name='b2g-commands',
+      version=PACKAGE_VERSION,
+      description='A set of mach commands to make working with the B2G repo easier.',
+      long_description='See https://github.com/ahal/b2g-commands',
+      classifiers=['Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+      keywords='mozilla',
+      author='Andrew Halberstadt',
+      author_email='ahalberstadt@mozilla.com',
+      url='https://github.com/ahal/b2g-commands',
+      license='MPL 2.0',
+      packages=['commands'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=deps)
